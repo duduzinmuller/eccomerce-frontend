f36636bd3d7c69725b32c40ad20906d2fd22511d
Eduardo <eduardomuller989@gmail.com>
2025-04-04 16:07:49 -0400

    feat: add error messages to Login Pages and custom email validation

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -1,10 +1,15 @@
 import { BsGoogle } from 'react-icons/bs'
 import { FiLogIn } from 'react-icons/fi'
 import { useForm } from 'react-hook-form'
+import validator from 'validator'
 
+// Components
 import CustomButton from '../../components/custom-button/custom-button-component'
+import CustomInput from '../../components/custom-input/custom-input.component'
 import Header from '../../components/header/header.components'
+import InputErrorMessage from '../../components/input-error-message/input-error-message.component'
 
+// Styles
 import {
   LoginContainer,
   LoginContent,
@@ -12,13 +17,12 @@ import {
   LoginInputContainer,
   LoginSubtitle
 } from './login.styles'
-import CustomInput from '../../components/custom-input/custom-input.component'
 
 const LoginPage = () => {
   const {
     register,
-    formState: { errors },
-    handleSubmit
+    handleSubmit,
+    formState: { errors }
   } = useForm()
 
   const handleSubmitPress = (data: any) => {
@@ -33,8 +37,7 @@ const LoginPage = () => {
         <LoginContent>
           <LoginHeadline>Entre com a sua conta</LoginHeadline>
 
-          {/* Button */}
-          <CustomButton startIcon={<BsGoogle size={20} />}>
+          <CustomButton startIcon={<BsGoogle size={18} />}>
             Entrar com o Google
           </CustomButton>
 
@@ -46,28 +49,41 @@ const LoginPage = () => {
               hasError={!!errors?.email}
               placeholder='Digite seu e-mail'
               {...register('email', {
-                required: true
+                required: true,
+                validate: (value) => {
+                  return validator.isEmail(value)
+                }
               })}
             />
+
+            {errors?.email?.type === 'required' && (
+              <InputErrorMessage>O e-mail é obrigatório.</InputErrorMessage>
+            )}
+
+            {errors?.email?.type === 'validate' && (
+              <InputErrorMessage>
+                Por favor, insira um e-mail válido.
+              </InputErrorMessage>
+            )}
           </LoginInputContainer>
+
           <LoginInputContainer>
             <p>Senha</p>
             <CustomInput
               hasError={!!errors?.password}
               placeholder='Digite sua senha'
-              {...register('password', {
-                required: true
-              })}
+              type='password'
+              {...register('password', { required: true })}
             />
+
+            {errors?.password?.type === 'required' && (
+              <InputErrorMessage>A senha é obrigatória.</InputErrorMessage>
+            )}
           </LoginInputContainer>
 
           <CustomButton
-            startIcon={
-              <FiLogIn
-                size={20}
-                onClick={() => handleSubmit(handleSubmitPress)()}
-              />
-            }
+            startIcon={<FiLogIn size={18} />}
+            onClick={() => handleSubmit(handleSubmitPress)()}
           >
             Entrar
           </CustomButton>
