274e5f57bb1a1464b4ba0234343d56a228e03463
Eduardo <eduardomuller989@gmail.com>
2025-04-05 08:04:15 -0400

    feat: get categories from firestore db

--- src/components/categories/categories.component.tsx
+++ src/components/categories/categories.component.tsx
@@ -1,56 +1,28 @@
 import { useEffect, useState } from 'react'
 import Category from '../../types/category.types'
-import axios from 'axios'
-import env from '../../config/env.config'
 import CategoryItem from '../category-item/category-item.component'
 import { CategoriesContainer, CategoriesContent } from './categories.styles'
+import { getDocs, collection } from 'firebase/firestore'
+import { db } from '../../config/firebase.config'
+import { categoryConverter } from '../../converters/firestore.converters'
 
 const Categories = () => {
-  const [categories, setCategories] = useState<Category[]>([
-    {
-      id: '6228f52eb7e6cb904bbe0111',
-      name: 'hats',
-      displayName: 'Chapéus',
-      imageUrl:
-        'https://images.unsplash.com/photo-1511231115599-3edad51208c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80'
-    },
-    {
-      id: '6228f760b7e6cb904bbe012e',
-      name: 'sneakers',
-      displayName: 'Tênis',
-      imageUrl:
-        'https://images.unsplash.com/photo-1552346154-21d32810aba3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
-    },
-    {
-      id: '6228fc5cb7e6cb904bbe014b',
-      name: 'jackets',
-      displayName: 'Jaquetas',
-      imageUrl:
-        'https://images.unsplash.com/photo-1601924994987-69e26d50dc26?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-    },
-    {
-      id: '6228fdd8b7e6cb904bbe0162',
-      name: 'female',
-      displayName: 'Feminino',
-      imageUrl:
-        'https://images.unsplash.com/photo-1597586124394-fbd6ef244026?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
-    },
-    {
-      id: '623064ee22376f1e6b869471',
-      name: 'male',
-      displayName: 'Masculino',
-      imageUrl:
-        'https://images.pexels.com/photos/1182825/pexels-photo-1182825.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'
-    }
-  ])
+  const [categories, setCategories] = useState<Category[]>([])
 
   console.log({ categories })
 
   const fetchCategories = async () => {
     try {
-      const { data } = await axios.get(`${env.apiUrl}/api/category`)
+      const categoriesFromFirestore: Category[] = []
+      const querySnapshhot = await getDocs(
+        collection(db, 'categories').withConverter(categoryConverter)
+      )
 
-      setCategories(data)
+      querySnapshhot.forEach((doc) => {
+        categoriesFromFirestore.push(doc.data())
+      })
+
+      setCategories(categoriesFromFirestore)
     } catch (error) {
       console.log(error)
     }
--- src/types/category.types.ts
+++ src/types/category.types.ts
@@ -1,8 +1,11 @@
+import Product from './products.types'
+
 interface Category {
   id: string
   name: string
   displayName: string
   imageUrl: string
+  product: Product[]
 }
 
 export default Category
