03e5075fecbaa5aa1ad6af01ece52a869fa8ff7d
Eduardo <eduardomuller989@gmail.com>
2025-04-08 14:51:31 -0400

    feat: add Loading to categories Overview

--- src/components/categories-overview/categories-overview.component.tsx
+++ src/components/categories-overview/categories-overview.component.tsx
@@ -2,9 +2,10 @@ import { FunctionComponent, useContext, useEffect } from 'react'
 import { Container } from './categories-overview.styles'
 import { CategoryContext } from '../../contexts/category.context'
 import CategoryOverview from '../category-overview/category-overview.component'
+import Loading from '../loading/loading.component'
 
 const CategoriesOverview: FunctionComponent = () => {
-  const { categories, fetchCategories } = useContext(CategoryContext)
+  const { categories, isLoading, fetchCategories } = useContext(CategoryContext)
 
   useEffect(() => {
     if (categories.length === 0) {
@@ -14,6 +15,7 @@ const CategoriesOverview: FunctionComponent = () => {
 
   return (
     <Container>
+      {isLoading && <Loading />}
       {categories.map((category) => (
         <CategoryOverview key={category.id} category={category} />
       ))}
